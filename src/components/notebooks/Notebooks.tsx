cbf5461cb28e15909e8f93826a466354564e06a0
khw970421 <khw970421@kakao.com>
2023-12-07 23:35:37 +0900

    Notebook 클릭 이벤트 처리

--- src/components/notebooks/Notebooks.tsx
+++ src/components/notebooks/Notebooks.tsx
@@ -1,6 +1,6 @@
 import { MouseEvent, useRef, useState } from "react";
 
-const Notebooks = () => {
+const Notebooks = ({ focusNotebook }: { focusNotebook: (notebook: string | null) => void }) => {
   const [notebooks, setNotebooks] = useState<string[]>([])
   const [isModalOpen, setIsModalOpen] = useState(false)
   const ref = useRef<HTMLInputElement>(null);
@@ -15,15 +15,21 @@ const Notebooks = () => {
   }
 
   const handleRemoveNoteBooks = (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation()
     const removeId = (event.target as HTMLElement).dataset.id || '';
     setNotebooks(notebooks => notebooks.filter((_, notebooksId) => String(notebooksId) !== removeId))
+    focusNotebook(null)
+  }
+
+  const handleOpenNotebook = (notebook: string) => {
+    focusNotebook(notebook)
   }
 
   return (
     <div>
-      {isModalOpen && <div><input ref={ref} /> <button onClick={handleCreateNoteBooks} /></div>}
+      {isModalOpen && <div><input ref={ref} /> <button onClick={handleCreateNoteBooks}>Create</button></div>}
       <div><span>NOTEBooks</span><button onClick={handleAddNoteBooks}>+</button></div>
-      {notebooks.map((notebook, id) => <div key={`${notebook}-${id}`}><span>{notebook}</span><button onClick={handleRemoveNoteBooks} data-id={id}>-</button></div>)}
+      {notebooks.map((notebook, id) => <div key={`${notebook}-${id}`} onClick={() => handleOpenNotebook(notebook)}><span>{notebook}</span><button onClick={handleRemoveNoteBooks} data-id={id}>-</button></div>)}
     </div>
   );
 };

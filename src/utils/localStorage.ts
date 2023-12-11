715d9acec00165a896379eb8b7a6f8beb8fe44bf
khw970421 <khw970421@kakao.com>
2023-12-11 12:19:55 +0900

    localStorage try catch 예외 처리

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,6 +1,17 @@
 const setItem = <T>(key: string, value: T) =>
   localStorage.setItem(key, JSON.stringify(value));
 
-const getItem = (key: string) => JSON.parse(localStorage.getItem(key) || 'null')
+const getItem = (key: string) => {
+  let localItem = null
+  try {
+    localItem = JSON.parse(localStorage.getItem(key) || 'null')
+    return localItem
+  }
+  catch (e) {
+    console.error('Something localStorage is wrong. remove item')
+    localStorage.removeItem(key)
+    return localItem
+  }
+}
 
 export { setItem, getItem };
